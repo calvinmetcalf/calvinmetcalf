ab09a9ce120884e64924b7570a1c78f13c750da0
Calvin Metcalf <cmetcalf@appgeo.com>
2018-12-21 08:44:53 -0500

    mah version

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-It's me, Tierney!
+It's me, Calvin! I forked this repo from Tierney and it's awesome!
 
 # Usage
 Via npx:
 ```
-npx bitandbang
+npx calvinmetcalf
 ```
 
 Not including how to use it globally because I'm not sure why you'd want this as a global command. That'd be creepy.
--- bin/card.js
+++ bin/card.js
@@ -14,14 +14,14 @@ const options = {
 
 // Text + chalk definitions
 const data = {
-  name: chalk.white('Tierney Cyren /'),
-  handle: chalk.cyan('bitandbang'),
-  work: chalk.white('Senior Cloud Developer Advocate at Microsoft'),
-  twitter: chalk.cyan('https://twitter.com/bitandbang'),
-  github: chalk.cyan('https://github.com/bnb'),
-  linkedin: chalk.cyan('https://linkedin.com/in/bitandbang'),
-  web: chalk.cyan('https://bnb.im'),
-  npx: chalk.white('npx bitandbang'),
+  name: chalk.white('Calvin Metcalf /'),
+  handle: chalk.cyan('cwmma'),
+  work: chalk.white('Web Developer at AppGeo'),
+  twitter: chalk.cyan('https://twitter.com/cwmma'),
+  github: chalk.cyan('https://github.com/calvinmetcalf'),
+  linkedin: chalk.cyan('https://linkedin.com/in/calvin-metcalf'),
+  web: chalk.cyan('https://calvinmetcalf.com'),
+  npx: chalk.white('npx calvinmetcalf'),
   labelWork: chalk.white.bold('      Work:'),
   labelTwitter: chalk.white.bold('   Twitter:'),
   labelGitHub: chalk.white.bold('    GitHub:'),
